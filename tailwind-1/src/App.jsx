78303be61d35536a2404cb723803769afd0ae619
Sharad Banga <sharadiit03@gmail.com>
2025-02-12 12:45:22 +0530

    first commit

---
+++ tailwind-1/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div class="bg-blue-900  h-screen w-screen flex justify-center items-center">
+
+        <div className="gg">
+
+            <div className="heading flex justify-center ">Webinar.gg</div>
+            <div className="tag flex justify-center">Verify Your Age</div>
+            <br />
+            <div className="tag1 flex justify-center">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+
+            <div className="in flex justify-center">
+            <input class="" type="text" />
+            </div>
+            <br />
+
+            <div className="btn in flex justify-center">
+            <button >send</button>
+            </div>
+            
+
+        </div>
+    </div>
+  )
+}
+
+export default App
