9076b54e5df6fa15ea127e76ba389ef85fbcd440
Sharad Banga <sharadiit03@gmail.com>
2025-02-14 22:26:08 +0530

    update

--- OTP/src/OTP.jsx
+++ OTP/src/OTP.jsx
@@ -18,25 +18,53 @@ export const OTP = ()=>{
   return(
     <div className="bg-blue-700 h-screen flex justify-center items-center flex-col">
     < div className=" flex gap-1 " >
-      <One reference={ref1} onDone={()=>{
+
+
+      <One reference={ref1} onDone={(e)=>{
         ref2.current.focus();
-      }} />
+      }}
+
+      onBackspace={()=>{
+        ref1.current.focus();
+      }}
+      
+      />
+
+
       <One reference={ref2} onDone={()=>{
         ref3.current.focus();
-      }}  />
+      }} 
+      onBackspace={()=>{
+        ref1.current.focus();
+      }}
+      />
+
+
       <One reference={ref3} onDone={()=>{
         ref4.current.focus();
+      }}
+      onBackspace={()=>{
+        ref2.current.focus();
       }} />
+
+
       <One reference={ref4} onDone={()=>{
         ref5.current.focus();
+      }}
+      onBackspace={()=>{
+        ref3.current.focus();
       }} />
+
+
       <One reference={ref5} onDone={()=>{
         setEnd(true);
-        
-      }} />
+      }} 
+      onBackspace={()=>{
+        ref4.current.focus();
+      }}/>
       
-    </div>
-    <br />
+        </div>
+        <br />
         
         <BTN end={end} fnn={()=>{
           alert("s");
@@ -47,12 +75,19 @@ export const OTP = ()=>{
 
 
 
-function One({reference,onDone}){
+function One({reference,onDone , onBackspace}){
   return(
     <>
       <input ref={reference} onChange={(e)=>{
         onDone();
-      }} className="w-[30px] h-[40px] bg-blue-500 outline-none px-2 text-white"
+      }} 
+      
+      onKeyDown={(e) => {
+        if (e.key === "Backspace" && !e.target.value) {
+          onBackspace();
+        }
+      }}
+      className="w-[30px] h-[40px] bg-blue-500 outline-none px-2 text-white"
       />
     </>
   )
