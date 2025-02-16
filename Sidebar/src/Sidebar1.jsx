931eb4b16af9e52216994ce53630ea8ecab25033
Sharad Banga <sharadiit03@gmail.com>
2025-02-17 03:53:22 +0530

    update

--- Sidebar/src/Sidebar1.jsx
+++ Sidebar/src/Sidebar1.jsx
@@ -6,10 +6,6 @@ export const Sidebar1 =()=>{
   return  (
     <div className="flex">
 
-        {/* <div className="bg-red-300 transition-all delay-2s w-0 md:w-96 h-screen">
-          Sidebar
-        </div>  */}
-
         <div className={`h-screen ${darki ? "dark:bg-green-600" : "bg-red-200"} flex-1`}>
         </div> 
 
